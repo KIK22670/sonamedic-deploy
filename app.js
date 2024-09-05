2fb1c0bb81bd89b57dc86c4ef02828dc47e71d32
KIK22670 <kikicaleksandra@gmail.com>
2024-09-05 15:06:54 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Statisches Verzeichnis für CSS, Bilder usw.
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route für die Hauptseite
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Server starten
+app.listen(PORT, () => {
+    console.log(`Server läuft auf http://localhost:${PORT}`);
+});
---
+++ public/script.js
@@ -0,0 +1,1 @@
+console.log('Sonamedic-Seite wurde erfolgreich geladen.');
