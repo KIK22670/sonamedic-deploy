0430548d69d61418f5f88d61f81c524d18bd3156
IrinaKostrba <kos22487@spengergasse.at>
2024-09-26 08:44:29 +0200

    audio test hingefügt

---
+++ public/hearingtest.js
@@ -0,0 +1,65 @@
+// Funktionen für die Tests
+function generalOnlineHearingTest() {
+    // Redirection to the general hearing test page
+    window.location.href = "generalHearingtest.html";
+}
+
+function pureToneAirConductionTest() {
+    alert("Pure-Tone-Air-Conduction Test wird gestartet");
+}
+
+function boneConductionAudiometry() {
+    alert("Bone-Conduction Audiometry wird gestartet");
+}
+
+function highFrequencyAudiometry() {
+    alert("High-Frequency Audiometry wird gestartet");
+}
+
+function freiburgSpeechTest() {
+    alert("Freiburger Sprachtest wird gestartet");
+}
+
+function oldenburgSentenceTest() {
+    alert("Oldenburger Satztest (OLSA) wird gestartet");
+}
+
+function goettingenSentenceTest() {
+    alert("Göttinger Satztest wird gestartet");
+}
+
+function loudnessDiscomfortLevelTest() {
+    alert("Lautstärkeschwellentest (Loudness Discomfort Level - LDL) wird gestartet");
+}
+
+function dynamicRangeTest() {
+    alert("Dynamischer Bereich Test wird gestartet");
+}
+
+function uclTest() {
+    alert("UCL (Uncomfortable Loudness Level) Test wird gestartet");
+}
+
+function directionHearingTest() {
+    alert("Richtungshörtest wird gestartet");
+}
+
+function binauralLocalization() {
+    alert("Binaurale Lokalisierung wird gestartet");
+}
+
+function binauralMaskingLevelDifference() {
+    alert("Binaurales Masking Level Difference (MLD) Test wird gestartet");
+}
+
+function gapDetectionTest() {
+    alert("Gap Detection Test wird gestartet");
+}
+
+function phonemeRecognitionTest() {
+    alert("Phonem Erkennungstest wird gestartet");
+}
+
+function temporalModulationTest() {
+    alert("Temporal Modulation Transfer Function (TMTF) Test wird gestartet");
+}
