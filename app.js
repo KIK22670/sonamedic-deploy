a9c2f4e450a07f8801c3b16f456751d1074308cb
KIK22670 <149530408+KIK22670@users.noreply.github.com>
2024-11-10 22:22:14 +0100

    Verifizierungstoken gefixxt + stammdatenformular + Reset Password fertiggestellt

--- app.js
+++ app.js
@@ -114,6 +114,97 @@ const emailVerificationTemplate = (verificationToken) => `
 </html>
 `;
 
+
+const resetPasswordTemplate = (token) => `
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Passwort zurücksetzen!</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+        }
+
+        .card {
+            border-radius: 10px;
+            overflow: hidden;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        }
+
+        .card-header {
+            background-color: #1b9aaa;
+            color: #fff;
+            padding: 20px;
+            border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+            text-align: center;
+        }
+
+        .card-body {
+            padding: 20px;
+            text-align: center;
+        }
+
+        h1 {
+            margin-top: 0;
+            font-size: 24px;
+        }
+
+        p {
+            margin-bottom: 20px;
+            color: #555;
+            line-height: 1.6;
+        }
+
+        .verification-link a {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #1b9aaa;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+            transition: background-color 0.3s ease;
+        }
+
+        .verification-link a:hover {
+            background-color: #0f7c8a;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h1>Passwort zurücksetzen</h1>
+            </div>
+            <div class="card-body">
+                <p>Bitte klicken Sie auf den folgenden Link, um Ihr Passwort zurückzusetzen</p>
+                <div class="verification-link">
+                <a href="http://localhost:3000/reset-password/${token}">Zurücksetzen</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+`;
+
 // Route for main page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'home.html'));
@@ -208,7 +299,7 @@ app.get('/verify-email/:token', async (req, res) => {
 
         // Update user as verified
         const updateUserQuery = {
-            text: 'UPDATE u_userverwaltung SET verified = true WHERE u_id = $1',
+            text: 'UPDATE u_userverwaltung SET u_verified = true WHERE u_id = $1',
             values: [user.u_id],
         };
 
@@ -242,13 +333,13 @@ app.post('/login', async (req, res) => {
         if (result.rows.length === 1) {
             const user = result.rows[0];
 
-            if (!user.verified) {
+            if (!user.u_verified) {
                 return res.status(401).json({ error: 'Please verify your email address before logging in' });
             }
 
             if (bcrypt.compareSync(passwort, user.u_passwort)) {
                 req.session.user = { id: user.u_id, email: user.u_email };
-                res.redirect('/doctorsearch');
+                res.redirect('/stammdaten');
             } else {
                 res.status(401).json({ error: 'Invalid email or password' });
             }
@@ -273,12 +364,87 @@ app.get('/logout', (req, res) => {
     });
 });
 
+
+app.get('/stammdaten', (req, res, next) => {
+    console.log("=======================================");
+    console.log(req.session.user);
+    console.log(req.session.user);
+    console.log("=======================================");
+    if (req.session.user) {
+
+        // User is logged in, proceed
+        res.sendFile(path.join(__dirname, 'public', 'stammdaten.html'));
+    } else {
+        // User is not logged in, redirect to login page
+        res.redirect('/login');
+    }
+});
+
+// POST-Anfrage zum Speichern oder Aktualisieren der Patientendaten
+app.post('/speichereStammdaten', async (req, res) => {
+    try {
+        const userID = req.session.user.id;
+        const { vorname, nachname, email, telefonnummer, svnr, allergien, vorerkrankungen, medikamente } = req.body;
+
+        // Überprüfen, ob bereits Patientendaten für diesen Benutzer vorhanden sind
+        const checkExistingDataQuery = {
+            text: 'SELECT * FROM p_patienten WHERE p_id = $1',
+            values: [userID],
+        };
+        const existingDataResult = await client.query(checkExistingDataQuery);
+
+        if (existingDataResult.rows.length > 0) {
+            // Es gibt bereits Patientendaten für diesen Benutzer, daher aktualisieren Sie sie
+            const updateDataQuery = {
+                text: `UPDATE p_patienten 
+               SET p_vorname = $1, p_nachname = $2, p_email = $3, p_telefonnummer = $4, 
+                   p_svnr = $5, p_allergien = $6, p_vorerkrankungen = $7, p_medikamente = $8, p_stammdaten = $9
+               WHERE p_id = $10`,
+                values: [vorname, nachname, email, telefonnummer, svnr, allergien, vorerkrankungen, medikamente, JSON.stringify(req.body), userID],
+            };
+            await client.query(updateDataQuery);
+        } else {
+            // Es gibt keine vorhandenen Patientendaten für diesen Benutzer, daher fügen Sie neue Daten hinzu
+            const insertDataQuery = {
+                text: `INSERT INTO p_patienten
+               (p_id, p_vorname, p_nachname, p_email, p_telefonnummer, p_svnr, p_allergien, p_vorerkrankungen, p_medikamente, p_stammdaten) 
+               VALUES 
+               ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
+                values: [userID, vorname, nachname, email, telefonnummer, svnr, allergien, vorerkrankungen, medikamente, JSON.stringify(req.body)],
+            };
+            await client.query(insertDataQuery);
+        }
+
+        res.status(201).json({ message: 'Patientendaten wurden gespeichert/aktualisiert' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get('/ladeStammdaten', async (req, res) => {
+    try {
+        const userID = req.session.user.id;
+        const result = await client.query('SELECT p_stammdaten FROM p_patienten WHERE p_id = $1', [userID]);
+
+        if (result.rows.length > 0 && result.rows[0].p_stammdaten) {
+            // Server: Senden Sie die Stammdaten als JSON-Zeichenfolgen
+            res.json({ success: true, stammdaten: JSON.stringify(result.rows[0].p_stammdaten) });
+        } else {
+            res.json({ success: false });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
 // Route to get patient data
 app.get('/api/patient/:id', async (req, res) => {
     const patientId = req.params.id;
     try {
         const query = {
-            text: 'SELECT p_vorname, p_nachname, p_geburtsdatum, p_geschlecht FROM p_patient WHERE p_id = $1',
+            text: 'SELECT p_vorname, p_nachname, p_geburtsdatum, p_geschlecht FROM p_patienten WHERE p_id = $1',
             values: [patientId],
         };
         const { rows } = await client.query(query);
@@ -329,6 +495,124 @@ app.get('/audios/:filename', (req, res) => {
     console.log(`Anfrage nach Audio-Datei: ${req.params.filename}`);
     res.sendFile(path.join(__dirname, 'public', 'audios', req.params.filename));
 });
+
+
+// Forgot Password validation
+function validateResetInput(data) {
+    let errors = {};
+
+    data.email = data.email.trim();
+
+    if (!data.email) {
+        errors.email = 'Email is required';
+    } else if (!/^\S+@\S+\.\S+$/.test(data.email)) {
+        errors.email = 'Email is invalid';
+    }
+
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0,
+    };
+}
+
+// Forgot Password Route
+app.post('/forgot-password', async (req, res) => {
+    const { errors, isValid } = validateResetInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const token = crypto.randomBytes(48).toString('hex');
+    const expirationTime = new Date(Date.now() + 3600000); // 1 hour expiration
+
+    try {
+        // Check if user with provided email exists
+        const userResult = await client.query('SELECT * FROM u_userverwaltung WHERE u_email = $1', [req.body.email]);
+        if (userResult.rows.length === 0) {
+            return res.status(400).json({ email: 'Invalid email address' });
+        }
+
+        // Save token and expiration date in database
+        await client.query(
+            'UPDATE u_userverwaltung SET u_resetpasswordtoken = $1, u_resetpasswordexpires = $2 WHERE u_email = $3',
+            [token, expirationTime, req.body.email]
+        );
+
+        // Send email to user
+        const mailOptions = {
+            to: req.body.email,
+            from: 'kikicaleksandra@gmail.com',
+            subject: 'Password Reset',
+            html: resetPasswordTemplate(token)
+        };
+        await sgMail.send(mailOptions);
+
+        res.status(200).sendFile(path.join(__dirname, 'public', 'resetpasswordverification.html'));
+    } catch (error) {
+        console.error('Error resetting password:', error);
+        res.status(500).json("Internal Server Error");
+    }
+});
+
+// Reset Password Route
+app.post('/reset-password/:token', async (req, res) => {
+    const { token } = req.params;
+    const { password, confirmPassword } = req.body;
+
+    if (password !== confirmPassword) {
+        return res.status(400).json({ confirmPassword: 'Passwords do not match' });
+    }
+
+    try {
+        // Check if the token is valid and not expired
+        const userResult = await client.query(
+            'SELECT * FROM u_userverwaltung WHERE u_resetpasswordtoken = $1 AND u_resetpasswordexpires > CURRENT_TIMESTAMP',
+            [token]
+        );
+
+        if (userResult.rows.length === 0) {
+            return res.status(400).json({ token: 'Token is invalid or has expired' });
+        }
+
+        // Update user password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await client.query(
+            'UPDATE u_userverwaltung SET u_passwort = $1, u_resetpasswordtoken = NULL, u_resetpasswordexpires = NULL WHERE u_resetpasswordtoken = $2',
+            [hashedPassword, token]
+        );
+
+        res.status(200).sendFile(path.join(__dirname, 'public', 'resetsuccess.html'));
+    } catch (error) {
+        console.error('Error resetting password:', error);
+        res.status(500).json("Internal Server Error");
+    }
+});
+
+// Reset Password Route
+app.get('/reset-password/:token', async (req, res) => {
+    const { token } = req.params;
+
+    try {
+        // Check if the token is valid and not expired
+        const userResult = await client.query(
+            'SELECT * FROM u_userverwaltung WHERE u_resetpasswordtoken = $1 AND u_resetpasswordexpires > CURRENT_TIMESTAMP',
+            [token]
+        );
+
+        if (userResult.rows.length === 0) {
+            return res.status(400).json({ token: 'Token is invalid or has expired' });
+        }
+
+        // If the token is valid, render the HTML page for resetting the password
+        res.sendFile(path.join(__dirname, 'public', 'newpassword.html'));
+    } catch (error) {
+        console.error('Error resetting password:', error);
+        res.status(500).json("Internal Server Error");
+    }
+});
+
+
 
 // Start server
 app.listen(PORT, () => {
---
+++ db.sql
@@ -0,0 +1,235 @@
+
+-- -----------------------------------------------------
+-- Table sonamedic.p_patienten
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS p_patienten (
+  p_id SERIAL PRIMARY KEY,
+  p_vorname VARCHAR(45) NULL,
+  p_nachname VARCHAR(45) NULL,
+  p_email VARCHAR(100),
+  p_telefonnummer VARCHAR(20) NULL,
+  p_geburtsdatum DATE NULL,
+  p_svnr VARCHAR(10) NULL,
+  p_allergien TEXT,
+  p_vorerkrankungen TEXT,
+  p_medikamente TEXT,
+  p_geschlecht SMALLINT NULL,
+  p_stammdaten JSON,
+  p_ist_patient SMALLINT NULL,
+  p_datenschutzerklärung SMALLINT NULL
+);
+
+-- -----------------------------------------------------
+-- Table sonamedic.u_userverwaltung
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS u_userverwaltung (
+  u_id SERIAL PRIMARY KEY,
+  u_username VARCHAR(45) NULL,
+  u_passwort VARCHAR(100) NULL,
+  u_email VARCHAR(80) NULL,
+  u_verified BOOLEAN,
+  u_verification_token VARCHAR(255),
+  u_resetpasswordtoken VARCHAR(255),
+  u_resetpasswordexpires TIMESTAMP WITHOUT TIME ZONE,
+  u_p_id INT REFERENCES p_patienten(p_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+-- -----------------------------------------------------
+-- Table sonamedic.l_logging
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS l_logging (
+  l_id SERIAL PRIMARY KEY,
+  l_event_typ VARCHAR(255) NULL,
+  l_timestamp TIMESTAMP NULL
+);
+
+-- -----------------------------------------------------
+-- Table sonamedic.tt_termintyp
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS tt_termintyp (
+  tt_id SERIAL PRIMARY KEY,
+  tt_bezeichnung VARCHAR(45) NULL
+);
+
+-- -----------------------------------------------------
+-- Table sonamedic.z_zeitslots
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS z_zeitslots (
+  z_id SERIAL PRIMARY KEY,
+  z_startzeit TIME NULL,
+  z_endzeit VARCHAR(45) NULL
+);
+
+-- -----------------------------------------------------
+-- Table sonamedic.t_termine
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS t_termine (
+  t_id SERIAL PRIMARY KEY,
+  t_datum DATE NULL,
+  t_uhrzeit TIME NULL,
+  t_termintyp VARCHAR(45) NULL,
+  t_p_id INT NOT NULL,
+  tt_termintyp_tt_id INT NOT NULL,
+  z_zeitslots_z_id INT NOT NULL,
+  CONSTRAINT fk_t_termine_p_patienten1 FOREIGN KEY (t_p_id)
+    REFERENCES p_patienten (p_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_t_termine_tt_termintyp1 FOREIGN KEY (tt_termintyp_tt_id)
+    REFERENCES tt_termintyp (tt_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_t_termine_z_zeitslots1 FOREIGN KEY (z_zeitslots_z_id)
+    REFERENCES z_zeitslots (z_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+-- -----------------------------------------------------
+-- Table sonamedic.h_hoertest
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS h_hoertest (
+  h_id SERIAL PRIMARY KEY,
+  h_testname VARCHAR(45) NULL,
+  h_beschreibung TEXT NULL,
+  h_kategorie VARCHAR(45) NULL,
+  h_erstellt_am TIMESTAMP NULL
+);
+
+-- -----------------------------------------------------
+-- Table sonamedic.b_benachrichtigungen
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS b_benachrichtigungen (
+  b_id SERIAL PRIMARY KEY,
+  b_nachricht TEXT NULL,
+  b_date TIMESTAMP NULL
+);
+
+-- -----------------------------------------------------
+-- Table sonamedic.sin_speech_in_noise_test
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS sin_speech_in_noise_test (
+  sin_id SERIAL PRIMARY KEY,
+  sin_datum DATE NULL,
+  sin_startzeit TIMESTAMP NULL,
+  sin_endzeit TIMESTAMP NULL,
+  sin_ergebnis DECIMAL(5,2) NULL,
+  sin_p_id INT NOT NULL,
+  CONSTRAINT fk_sin_speech_in_noise_test_p_patienten1 FOREIGN KEY (sin_p_id)
+    REFERENCES p_patienten (p_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+-- -----------------------------------------------------
+-- Table sonamedic.sine_ergebnisse
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS sine_ergebnisse (
+  ergebnis_id SERIAL PRIMARY KEY,
+  sine_richtigeAntworten INT NULL,
+  sine_falscheAntworten INT NULL,
+  sine_sin_id INT NOT NULL,
+  CONSTRAINT fk_sine_ergebnisse_sin_speech_in_noise_test1 FOREIGN KEY (sine_sin_id)
+    REFERENCES sin_speech_in_noise_test (sin_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+-- -----------------------------------------------------
+-- Table sonamedic.e_ergebnisse
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS e_ergebnisse (
+  e_id SERIAL PRIMARY KEY,
+  e_ergebnisse VARCHAR(255) NULL,
+  e_datum DATE NULL,
+  e_veraenderung DECIMAL(5,2) NULL,
+  e_p_id INT NOT NULL,
+  e_h_id INT NOT NULL,
+  e_b_id INT NOT NULL,
+  e_sine_id INT NOT NULL,
+  CONSTRAINT fk_e_ergebnisse_p_patienten1 FOREIGN KEY (e_p_id)
+    REFERENCES p_patienten (p_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_e_ergebnisse_h_hoertest1 FOREIGN KEY (e_h_id)
+    REFERENCES h_hoertest (h_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_e_ergebnisse_b_benachrichtigungen1 FOREIGN KEY (e_b_id)
+    REFERENCES b_benachrichtigungen (b_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_e_ergebnisse_sine_ergebnisse1 FOREIGN KEY (e_sine_id)
+    REFERENCES sine_ergebnisse (ergebnis_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+-- -----------------------------------------------------
+-- Table sonamedic.pl_p_has_l
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS pl_p_has_l (
+  pl_p_id INT NOT NULL,
+  pl_l_id INT NOT NULL,
+  PRIMARY KEY (pl_p_id, pl_l_id),
+  CONSTRAINT fk_p_patienten_has_l_logging_p_patienten1 FOREIGN KEY (pl_p_id)
+    REFERENCES p_patienten (p_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_p_patienten_has_l_logging_l_logging1 FOREIGN KEY (pl_l_id)
+    REFERENCES l_logging (l_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+-- -----------------------------------------------------
+-- Table sonamedic.pb_p_hat_b
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS pb_p_hat_b (
+  pb_p_id INT NOT NULL,
+  pb_b_id INT NOT NULL,
+  PRIMARY KEY (pb_p_id, pb_b_id),
+  CONSTRAINT fk_p_patienten_has_b_benachrichtigungen_p_patienten1 FOREIGN KEY (pb_p_id)
+    REFERENCES p_patienten (p_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_p_patienten_has_b_benachrichtigungen_b_benachrichtigungen1 FOREIGN KEY (pb_b_id)
+    REFERENCES b_benachrichtigungen (b_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+-- -----------------------------------------------------
+-- Table sonamedic.sinf_frage
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS sinf_frage (
+  sinf_id SERIAL PRIMARY KEY,
+  sinf_frage VARCHAR(255) NULL,
+  sinf_erwarteteAntwort VARCHAR(255) NULL,
+  sinf_hintergrundlautstaerke DECIMAL(5,2) NULL,
+  sinf_soundlautstaerke DECIMAL(5,2) NULL,
+  sinf_sin_id INT NOT NULL,
+  CONSTRAINT fk_sinf_frage_sin_speech_in_noise_test1 FOREIGN KEY (sinf_sin_id)
+    REFERENCES sin_speech_in_noise_test (sin_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+-- -----------------------------------------------------
+-- Table sonamedic.sina_antwort
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS sina_antwort (
+  sina_id SERIAL PRIMARY KEY,
+  sina_antwort TEXT NULL,
+  sina_ist_korrekt SMALLINT NULL,
+  sina_sinf_id INT NOT NULL,
+  sina_p_id INT NOT NULL,
+  CONSTRAINT fk_sina_antwort_sinf_frage1 FOREIGN KEY (sina_sinf_id)
+    REFERENCES sinf_frage (sinf_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_sina_antwort_p_patienten1 FOREIGN KEY (sina_p_id)
+    REFERENCES p_patienten (p_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
