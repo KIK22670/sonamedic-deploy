c88845d93917abadfbfb395b3c59d3800046b885
KIK22670 <149530408+KIK22670@users.noreply.github.com>
2025-02-16 14:52:28 +0100

    db deploy

--- connection.js
+++ connection.js
@@ -1,16 +1,16 @@
-const { Client } = require('pg')
+require('dotenv').config(); // Umgebungsvariablen aus der .env-Datei laden
+const { Client } = require('pg');
 
 const client = new Client({
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: "rootUser",
-    database: "postgres"
-})
-
-module.exports = client
-client.connect()
-    .then(() => console.log('Connected to the database'))
-    .catch(error => console.error('Error connecting to the database', error));
+  host: process.env.PG_HOST,         // Render-Host
+  port: process.env.PG_PORT,         // Standard-Port 5432
+  user: process.env.PG_USER,         // Render-Benutzername
+  password: process.env.PG_PASSWORD, // Render-Passwort
+  database: process.env.PG_DATABASE  // Render-Datenbankname
+});
 
 module.exports = client;
+
+client.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch(error => console.error('Error connecting to the database', error));
